17c90c42b215b96b3e3fd971ce6ccab48a6cbb71
bonsai <onsen.bonsai@gmail.com>
2026-09-15 16:25:48 +0900

    feat: add aki service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='aki-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./data/days.csv','./data/participants.csv','./data/stars.csv'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const copy=x.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return x})))));
